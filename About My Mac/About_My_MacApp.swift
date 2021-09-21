aa5a7484b02d2fd0b1a75e0e0066b6ed958a5fe8
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-09-20 20:05:58 -0600

    Initial Commit

---
+++ About My Mac/About_My_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_My_MacApp.swift
+//  About My Mac
+//
+//  Created by Ben Sova on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct About_My_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About My MacTests/About_My_MacTests.swift
@@ -0,0 +1,33 @@
+//
+//  About_My_MacTests.swift
+//  About My MacTests
+//
+//  Created by Ben Sova on 9/20/21.
+//
+
+import XCTest
+@testable import About_My_Mac
+
+class About_My_MacTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
